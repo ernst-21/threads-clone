9391ec489b7c439e05eff996d7371ab23b976ed4
Ernesto Rivera <riverag8504@gmail.com>
2023-08-30 11:53:06 -0400

    [feature] created thread details and comments

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import Thread from '../models/thread.model';
 import User from '../models/user.model';
 import { connectToDB } from '../mongoose';
+import mongoose from 'mongoose';
 
 interface Params {
 	text: string;
@@ -71,4 +72,74 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 	} catch (error: any) {
 		throw new Error(`Failed to fetch threads: ${error.message}`);
 	}
+}
+
+export async function fecthThreadById(id: string) {
+	connectToDB();
+
+	try {
+		// TODO: populate community
+		const thread = await Thread.findById(id)
+			.populate({
+				path: 'author',
+				model: User,
+				select: '_id id name image',
+			})
+			.populate({
+				path: 'children',
+				populate: [
+					{
+						path: 'author',
+						model: User,
+						select: '_id id name parentId image',
+					},
+					{
+						path: 'children',
+						model: Thread,
+						populate: {
+							path: 'author',
+							model: User,
+							select: '_id id name parentId image',
+						},
+					},
+				],
+			})
+			.exec();
+
+		return thread;
+	} catch (error: any) {
+		throw new Error(`Failed to find thread ${error.message}`);
+	}
+}
+
+export async function addCommentToThread(
+	threadId: string,
+	commentText: string,
+	userId: string,
+	path: string
+) {
+	connectToDB();
+
+	try {
+		// Fin orginal Thread by its Id
+		const originalThread = await Thread.findById(threadId);
+
+		if (!originalThread) {
+			throw new Error('No thread found for this comment');
+		}
+
+		// Create a new thread with the comment text
+		const commentThread = new Thread({
+			text: commentText,
+			author: userId,
+			parentId: threadId,
+		});
+		// save the new thread
+		const savedCommentThread = await commentThread.save();
+
+		originalThread.children.push(savedCommentThread._id);
+
+		await originalThread.save();
+		revalidatePath(path);
+	} catch (error: any) {}
 }
